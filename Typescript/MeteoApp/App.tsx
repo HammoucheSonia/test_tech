71f0c4cbf3e5e619b2d42a609b73aa93b7d0e2a8
HammoucheSonia <soniiahamm@gmail.com>
2023-07-25 03:27:41 +0200

    first

---
+++ Typescript/MeteoApp/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>Welcome to React Native!</Text>
+      <Text>Welcome to React Native!</Text>
+      <Text>Welcome to React Native!</Text>
+      <Text>Welcome to React Native!</Text>
+    </View>
+  );
+};
+
+export default App;
